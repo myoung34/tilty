7f8b603d7c0cc357f91c5a6fdb656a9c25972d74
Marcus Young <myoung34@my.apsu.edu>
2020-02-13 22:27:34 -0600

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Marcus Young',
     author_email='3vilpenguin@gmail.com',
     py_modules=['tilty', 'blescan'],
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
         'Click',
