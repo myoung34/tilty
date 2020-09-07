accb989a9b29e5bc2327f195ae3b0353ca0138ca
nalabelle <nalabelle@gmail.com>
2020-09-07 11:34:37 -0700

    clarify test for delay_minutes in webhook emitter

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -123,6 +123,10 @@ def test_webhook_delay_minutes(
         'method': 'GET',
     }
     wh = webhook.Webhook(config=config)
+    # On init, we load delay_minutes from config
+    assert wh.delay_minutes == 3
+    # delay_until is unset until emitting calling emit once
+    assert wh.delay_until is None
     wh.emit({
         'color': 'black',
         'gravity': 1,
@@ -139,8 +143,9 @@ def test_webhook_delay_minutes(
     })
     now = datetime.datetime.now(datetime.timezone.utc)
     assert wh.delay_minutes == 3
-    assert wh.delay_until is not None
-    assert wh.delay_until >= now
+    # delay_until should be set for about 3 minutes from now
+    assert wh.delay_until is not None and wh.delay_until >= now
+    # emitted twice, but the second returned before actually sending a request.
     assert mock_requests.mock_calls == [
         mock.call.get('GET'),
         mock.ANY,
@@ -149,14 +154,19 @@ def test_webhook_delay_minutes(
             json={'color': 'black', 'gravity': 1, 'temp': 32}, url='http://example.com')  # noqa
     ]
 
+    # move the clock forward by setting delay_until to the past, which should
+    # allow a request to process again
     wh.delay_until = now - datetime.timedelta(minutes=1)
     wh.emit({
         'color': 'black',
-        'gravity': 2,
-        'temp': 33,
+        'gravity': 3,
+        'temp': 34,
         'mac': '00:0a:95:9d:68:16',
         'timestamp': 155558899
     })
+    # delay_until is once again about 3 minutes in the future
+    assert wh.delay_until is not None and wh.delay_until >= now
+    # we now see the request that was made after the delay timeout
     assert mock_requests.mock_calls == [
         mock.call.get('GET'),
         mock.ANY,
@@ -166,5 +176,5 @@ def test_webhook_delay_minutes(
         mock.ANY,
         mock.call.get()(
             headers={'Content-Type': 'application/json'},
-            json={'color': 'black', 'gravity': 2, 'temp': 33}, url='http://example.com')  # noqa
+            json={'color': 'black', 'gravity': 3, 'temp': 34}, url='http://example.com')  # noqa
     ]
