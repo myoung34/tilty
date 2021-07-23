7c0b60a99a67869d9028837071cd7744b078584c
Marcus Young <myoung34@my.apsu.edu>
2021-07-23 03:53:15 +0000

    Bump version 0.11.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     py_modules=['tilty', 'blescan'],
-    version='0.10.3',
+    version='0.11.0',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
         'Click',
